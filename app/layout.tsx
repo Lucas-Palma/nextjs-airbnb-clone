0eec7c207b13bd500cd1970329457702996da3ab
Lucas-Palma <palmalucassantiago@gmail.com>
2023-05-09 19:02:06 -0300

    add: login functionallity

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,8 @@ import { Nunito } from "next/font/google";
 import ClientOnly from './components/ClientOnly';
 import RegisterModal from './components/modals/RegisterModal';
 import ToasterProvider from './providers/ToasterProvider';
+import LoginModal from './components/modals/LoginModal';
+import getCurrentUser from './actions/getCurrentActions';
 
 export const metadata = {
   title: 'Airbnb clone',
@@ -15,18 +17,20 @@ const font = Nunito({
   }
 )
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
+  const currentUser = await getCurrentUser();
   return (
     <html lang="en">
       <body className={font.className}>
         <ClientOnly>
           <ToasterProvider />
           <RegisterModal />
-          <Navbar />
+          <LoginModal />
+          <Navbar currentUser={currentUser} />
         </ClientOnly>
         {children}
       </body>
